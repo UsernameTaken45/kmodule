f013bf0dfc0acfe2dfe08a2fb90749f80ba7749c
UsernameTaken45 <xander.c.pronk@gmail.com>
2024-11-20 20:07:31 +0100

    inital commit

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+// Created by xander on 20-11-24.
+//
+#include <linux/module.h>
+#include <linux/init.h>
+
+int module_load(void) {
+    kprint("Module loaded\n");
+    return 0;
+}
+
+void module_unload(void){
+    printk("module unloaded.\n");
+    return;
+}
+
+module_init(module_load);
+module_exit(module_unload);
+
+
+MODULE_LICENSE("GPL");
