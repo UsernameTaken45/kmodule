e5536047755d62646839731484f48dd32739f069
UsernameTaken45 <xander.c.pronk@gmail.com>
2024-11-20 20:57:14 +0100

    Register character file

--- kmodule.c
+++ kmodule.c
@@ -6,12 +6,42 @@
 
 
 #include "data.h"
-int module_load(void) {
-    printk(load_text);
+
+// Called when the driver file is opened.
+static int file_open(struct inode* device_file, struct file* instance){
+    printk("Device file opened\n");
     return 0;
 }
 
-void module_unload(void){
+// Called when the driver file is closed.
+static int file_close(struct inode* device_file, struct file* instance){
+    printk("Device file closed\n");
+    return 0;
+}
+
+static struct file_operations fops = {
+        .owner = THIS_MODULE,
+        .open = file_open,
+        .release = file_close
+};
+
+// Called on module load
+static int module_load(void) {
+
+    int ret;
+    ret = register_chrdev(90, "kmodule", fops);
+    if (ret == 0){
+        printk(load_text);
+    } else if (ret > 0){
+        printk("Number was already registered?\n");
+    } else return -1;
+
+    return 0;
+}
+
+// Called on module unload
+static void module_unload(void){
+    unregister_chrdev(90, "kmodule");
     printk("module unloaded.\n");
     return;
 }
@@ -21,3 +51,4 @@ module_exit(module_unload);
 
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Test module for playing around");
